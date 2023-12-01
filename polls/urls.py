025d35a5d7e79111251b5f9bf88cb84db1640597
unknown <annica.helsingius@gmail.com>
2023-12-01 12:20:07 +0200

    Initial commit

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin, auth
+from django.urls import path, include
+
+from . import views
+from .views import deleteView
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('delete/', deleteView, name='delete'),
+]
